19e0187eff75d1f769026fe4bae202617ff92f57
RakeMeUp <bencehetes@gmail.com>
2023-04-01 02:09:32 +0200

    init commit

---
+++ src/main/java/com/rake/gisbackend/GisbackendApplication.java
@@ -0,0 +1,13 @@
+package com.rake.gisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GisbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GisbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rake/gisbackend/GisbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rake.gisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GisbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

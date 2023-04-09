62fb7ff19bc4d4335123ec0f7e6826d15892dace
Hetes Sándor Bence <bencehetes@gmail.com>
2023-04-09 02:58:14 +0200

    cleanup

--- src/main/java/com/rake/gisbackend/service/EntryService.java
+++ src/main/java/com/rake/gisbackend/service/EntryService.java
@@ -1,13 +1,8 @@
 package com.rake.gisbackend.service;
 
 import com.rake.gisbackend.model.Entry;
-import com.rake.gisbackend.model.PostPayload;
 import com.rake.gisbackend.repository.EntryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class EntryService extends BaseCrudService<Entry, EntryRepository>{
